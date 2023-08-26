650d20d2840d035c687c664e8b415bcc34bb2508
Sharath Chandra <csharathchandra3@gmail.com>
2023-08-26 15:24:52 +0530

    responsive

--- client/src/Pages/Incomplete Teams/teams.js
+++ client/src/Pages/Incomplete Teams/teams.js
@@ -1,82 +1,104 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 import { api_url } from "../../config";
 
-
 const Teams = () => {
-    const[data,setTeams]=useState([]);
-    const navigate=useNavigate();
-    const url = api_url + '/register/fetch';
-   
-    const fetchTeams=()=>{
-      axios.post(url,{
-        message:"hi"
+  const [data, setTeams] = useState([]);
+  const navigate = useNavigate();
+  const url = api_url + "/register/fetch";
+
+  const fetchTeams = () => {
+    axios
+      .post(url, {
+        message: "hi",
       })
-      .then(res => {
-              console.log(res);
-              if (res.data.status) {
-                setTeams(res.data.Teams)
-              }
-            }
-            )
-    }
+      .then((res) => {
+        console.log(res);
+        if (res.data.status) {
+          setTeams(res.data.Teams);
+        }
+      });
+  };
 
+  useEffect(() => {
+    fetchTeams();
+  }, []);
 
-    useEffect(()=>{
-        fetchTeams();
-    },[])
-    
-        
-
-
-  return(
-      <div className="wrapper">
-        <div className="two" id="two">
-            <h1 id="registration" className="reg" style={{margin:"2% 0% 0% 0%",letterSpacing:"0px",fontWeight:"1500"}}>Available Teams</h1>
-            <div className="regform" style={{display:"flex",float:"left",justifyContent:"start",margin:"-4.1% 5% 1% 4%"}}>
-            <div className="formrow btcenter">
-              <div className="container" id="container">
-                <button class="learn-more" onClick={() => navigate('/register')}>
-                  <span class="circle" aria-hidden="true">
-                    <span class="icon arrow"></span>
-                  </span>
-                  <span class="button-text">Home</span>
-                </button>
-              </div>
+  return (
+    <div className="wrapper">
+      <div className="two" id="two">
+        <h1
+          id="registration"
+          className="reg"
+          style={{
+            margin: "2% 0% 0% 0%",
+            letterSpacing: "0px",
+            fontWeight: "1500",
+          }}
+        >
+          Available Teams
+        </h1>
+        <div
+          className="regform"
+          style={{
+            display: "flex",
+            float: "left",
+            justifyContent: "start",
+            margin: "-4.1% 5% 1% 4%",
+          }}
+        >
+          <div className="formrow btcenter">
+            <div className="container" id="container">
+              <button class="learn-more" onClick={() => navigate("/register")}>
+                <span class="circle" aria-hidden="true">
+                  <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Home</span>
+              </button>
             </div>
           </div>
+        </div>
 
-            <div className="leaderboardcont" style={{height:"110px",marginBottom:"0%",overflow:"hidden"}}>
-            <div className="team" style={{border:"none",boxShadow:"none"}}>
-                <h5 className="teamname">TeamName</h5>
-                <h5 className="teamname" style={{width:"25%"}}>{`Required Tech Stack`}</h5>
-                <h5 className="teamname"style={{width:"25%"}}>{`Contact`}</h5>
-                <h5 className="pts" >{`Members`}</h5>
-                <h5 className="time" style={{width:"15%",lineHeight:"27px"}}>{'At least 1 Female Member'}</h5>
-            </div>        
-            </div>
-            <div className="leaderboardcont">
-                          
-                {data.map((e)=>{
-                  return(
-
-                    <div className="team">
-                        <h5 className="teamname">{e.TeamName}</h5>
-                        <h5 className="teamname">{`${e.TechStack}`}</h5>
-                        <h5 className="teamname"style={{width:"25%",fontSize:"18px"}}>{`+91 ${e.PhoneNumber}`}</h5>
-                        <h5 className="pts" >{`${e.Members.length}/6`}</h5>
-                        <h5 className="time" style={{width:"20%"}}>{e.female?"Yes":"No "}</h5>
-                    </div>            
-                      )
-                }) 
-            }
-            </div>
+        <div
+          className="leaderboardcont"
+          style={{ height: "110px", marginBottom: "0%", overflow: "hidden" }}
+        >
+          <div className="team" style={{ border: "none", boxShadow: "none" }}>
+            <h5 className="teamname">TeamName</h5>
+            <h5
+              className="teamname"
+              style={{ width: "25%" }}
+            >{`Required Tech Stack`}</h5>
+            <h5 className="teamname" style={{ width: "25%" }}>{`Contact`}</h5>
+            <h5 className="pts">{`Members`}</h5>
+            <h5 className="time" style={{ width: "15%", lineHeight: "27px" }}>
+              {"At least 1 Female Member"}
+            </h5>
+          </div>
+        </div>
+        <div className="leaderboardcont">
+          {data.map((e) => {
+            return (
+              <div className="team">
+                <h5 className="teamname">{e.TeamName}</h5>
+                <h5 className="teamname">{`${e.TechStack}`}</h5>
+                <h5
+                  className="teamphone"
+                  style={{ width: "25%" }}
+                >{`+91 ${e.PhoneNumber}`}</h5>
+                <h5 className="pts">{`${e.Members.length}/6`}</h5>
+                <h5 className="time" style={{ width: "20%" }}>
+                  {e.female ? "Yes" : "No "}
+                </h5>
+              </div>
+            );
+          })}
         </div>
       </div>
-  )
+    </div>
+  );
+};
 
-}
-
-export default Teams
+export default Teams;

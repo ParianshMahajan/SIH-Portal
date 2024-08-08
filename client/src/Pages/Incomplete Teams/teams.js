0f14778b778bf6830a90e0d02b797612fa541cb6
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-08-08 21:35:35 +0530

    add responsiveness to table

--- client/src/Pages/Incomplete Teams/teams.js
+++ client/src/Pages/Incomplete Teams/teams.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useCookies } from "react-cookie";
 import { api_url } from "../../config";
 
 const Teams = () => {
@@ -49,42 +48,41 @@ const Teams = () => {
             margin: "-4.1% 5% 1% 4%",
           }}
         >
-          <div className="formrow btcenter ">
+          <div className="formrow btcenter">
             <div className="container" id="container">
               <div className="hister">
-
-              <button class="learn-more" onClick={() => navigate("/register")}>
-                <span class="circle" aria-hidden="true">
-                  <span class="icon arrow"></span>
-                </span>
-                <span class="button-text">Home</span>
-              </button>
+                <button className="learn-more" onClick={() => navigate("/register")}>
+                  <span className="circle" aria-hidden="true">
+                    <span className="icon arrow"></span>
+                  </span>
+                  <span className="button-text">Home</span>
+                </button>
               </div>
             </div>
           </div>
         </div>
 
         <div className="kuchbhi">
-  <div className="leaderboardcont">
-    <div className="team" style={{ border: "none", boxShadow: "none" }}>
-      <div className="teamname">TeamName</div>
-      <div className="teamname">Tech Stack</div>
-      <div className="teamname">Contact</div>
-      <div className="pts">Members</div>
-      <div className="time">Female Member</div>
-    </div>
-    {data.map((e) => (
-      <div className="team" key={e.TeamName}>
-        <div className="teamname">{e.TeamName}</div>
-        <div className="teamname">{e.TechStack}</div>
-        <div className="teamname">{e.PhoneNumber}</div>
-        <div className="pts">{`${e.Members.length}/6`}</div>
-        <div className="time">{e.female ? "Yes" : "No"}</div>
+          <div className="leaderboardcont">
+            <div className="team" style={{ border: "none", boxShadow: "none" }}>
+              <div className="teamname">TeamName</div>
+              <div className="teamname">Tech Stack</div>
+              <div className="teamname">Contact</div>
+              <div className="pts">Members</div>
+              <div className="time">Female Member</div>
+            </div>
+            {data.map((e) => (
+              <div className="team" key={e.TeamName}>
+                <div className="teamname">{e.TeamName}</div>
+                <div className="teamname">{e.TechStack}</div>
+                <div className="teamname">{e.PhoneNumber}</div>
+                <div className="pts">{`${e.Members.length}/6`}</div>
+                <div className="time">{e.female ? "Yes" : "No"}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    ))}
-  </div>
-</div>
-    </div>
     </div>
   );
 };
